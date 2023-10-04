27393a5e6e34febf58d45347ab485921c1ec731b
SebLay <34314258+BliZmuth@users.noreply.github.com>
2023-10-04 13:50:26 +0200

    Zombie walks MOUHAHAHAHA

---
+++ Ennemies/Zombie/zombie.cs
@@ -0,0 +1,72 @@
+using Godot;
+using System;
+
+public partial class zombie : CharacterBody2D
+{
+    
+    [Export]
+	private float MoveSpeed {get;set;} = 100;
+
+    [Export]
+    private Node2D Player {get;set;}
+
+    [Export]
+	private Vector2 StartingDirection {get;set;} = new Vector2(0,1);
+
+    private Timer PathFindTimer {get;set;}
+
+
+    private NavigationAgent2D NavAgent{get;set;}
+
+	private AnimationTree AnimationTree{get;set;}
+	private AnimationNodeStateMachinePlayback StateMachine{get;set;}
+
+    public override void _Ready()
+    {
+        NavAgent = GetNode<NavigationAgent2D>("NavigationAgent2D");
+
+        AnimationTree = GetNode<AnimationTree>("AnimationTree");
+		AnimationTree.Set("parameters/Idle/blend_position",StartingDirection);
+
+		StateMachine = (AnimationNodeStateMachinePlayback)AnimationTree.Get("parameters/playback");
+
+        MakePath();
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        var direction = ToLocal(NavAgent.GetNextPathPosition()).Normalized();
+        
+        Velocity = direction * MoveSpeed;
+
+        UpdateAnimationParameters(direction);
+
+        MoveAndSlide();
+
+        PickNewState();
+    }
+
+	public void UpdateAnimationParameters(Vector2 MoveInput){
+		if(MoveInput != Vector2.Zero){
+			AnimationTree.Set("parameters/Walk/blend_position", MoveInput);
+			// Will be used if a add other idle animation.
+			AnimationTree.Set("parameters/Idle/blend_position", MoveInput);
+		}
+	}
+	public void PickNewState(){
+		if(Velocity != Vector2.Zero){
+			StateMachine.Travel("Walk");
+		}else{
+			StateMachine.Travel("Idle");
+		}
+	}
+
+    private void MakePath(){
+        NavAgent.TargetPosition = Player.GlobalPosition;
+    }
+
+    private void OnTimerTimeout(){
+        MakePath();
+    }
+
+}
