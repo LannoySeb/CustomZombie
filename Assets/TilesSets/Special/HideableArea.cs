0ba481e411fbf400f8963a912bf980b59f678e41
SebLay <34314258+BliZmuth@users.noreply.github.com>
2023-10-08 14:18:09 +0200

    clear print

--- Assets/TilesSets/Special/HideableArea.cs
+++ Assets/TilesSets/Special/HideableArea.cs
@@ -4,7 +4,6 @@ using System;
 public partial class HideableArea : Node2D
 {
     public void OnBodyEntered(Node2D body){
-        GD.Print(body.GetGroups());
         if(body.IsInGroup("Player")){
             Hide();
         }
